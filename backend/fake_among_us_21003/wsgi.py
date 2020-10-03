abefe8aa728e94d077a6939efca0088ec5c70092
Crowdbotics <team@crowdbotics.com>
2020-10-03 02:32:23 +0000

    Initial commit.

---
+++ backend/fake_among_us_21003/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fake_among_us_21003 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fake_among_us_21003.settings')
+
+application = get_wsgi_application()
